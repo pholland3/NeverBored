19f78d682b6d0cfe70028d4e818c0d01cd1435ac
Peyton Holland <pholland3@ggc.edu>
2024-11-27 18:04:07 -0500

    Commit

---
+++ src/test/java/org/example/neverbored/NeverBoredApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.neverbored;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeverBoredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
